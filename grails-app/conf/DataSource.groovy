ca8b1505358a6f728126d8b5a75f677ed937ca85
Manij Shrestha <manijshrestha@gmail.com>
2013-02-02 15:35:43 -0600

    WIP: Added MySQL

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,8 +13,11 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/ziptagcloud"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "ziptagcloud"
+			password = "ziptagcloud"
         }
     }
     test {
